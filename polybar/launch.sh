4666551a62d9e7799037a3ec1d31d90f8a04e5d2
JodiMcJodson <83870316+DaQueenJodi@users.noreply.github.com>
2022-07-27 15:13:45 -0500

    new file:   bspwm/bspwmrc 	new file:   kitty/kitty.conf 	new file:   polybar/config.ini 	new file:   polybar/launch.sh 	new file:   rofi/config.rasi 	new file:   sxhkd/sxhkdrc

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+MONITOR=HDMI-A-3 polybar secondary 2>&1 | tee -a /tmp/polybar.log & disown
+MONITOR=DVI-D-0 polybar primary 2>&1 | tee -a /tmp/polybar.log & disown
+echo "Polybar launched..."
